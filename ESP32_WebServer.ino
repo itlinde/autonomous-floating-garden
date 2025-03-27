7c2c73e87a088e6382b2c827a229c8154b980244
Isabella <76260365+itlinde@users.noreply.github.com>
2025-03-26 23:07:27 -0700

    integrated html and css into web server ui

--- ESP32_WebServer.ino
+++ ESP32_WebServer.ino
@@ -1,6 +1,6 @@
 /* ======================================== 
 #### NOTES:
-- make sure laptop is connected to ubcvisitor before running!
+- make sure laptop is connected to hot spot before running!
 
 #### TROUBLESHOOTING: 
 - if upload error 2, make sure upload speed = 112500
@@ -26,10 +26,16 @@ String header;
 
 // auxiliar variables to store current output states
 String pumpState = "off";
+String soilState = "wet"; // "wet": moisture levels over threshold
+                          // "dry": moisture levels under threshold
 
 // output variables for GPIO pins
-const int output25 = 25;
-const int output32 = 32;
+const int pumpOutput = 25;
+const int sensorInput = 32;
+
+int moistureReading = 0;
+float moisturePercent = 0;
+int threshold = 429;
 
 // Current time
 unsigned long currentTime = millis();
@@ -66,6 +72,18 @@ void setup() {
 void loop(){
   WiFiClient client = server.available();   // Listen for incoming clients
 
+  // saturation level calcs
+  moistureReading = analogRead(sensorInput);
+  Serial.print("Moisture sensor reading: ");
+  Serial.println(moistureReading);
+  if (moistureReading > threshold) { // higher reading = drier soil
+    soilState = "dry";
+  } else {
+    soilState = "wet";
+  }
+  // moisture percent:
+  moisturePercent = 100 * (1 - (moistureReading - 30) / (800 - 30));
+
   if (client) {                             // If a new client connects,
     currentTime = millis();
     previousTime = currentTime;
@@ -90,33 +108,179 @@ void loop(){
 
             // HTTPS routes
             if (header.indexOf("GET /25/on") >= 0) {
-              Serial.println("GPIO 25 on");
+              Serial.println("pump on");
               pumpState = "on";
-              digitalWrite(output26, HIGH);
-            } else if (header.indexOf("GET /26/off") >= 0) {
-              Serial.println("GPIO 26 off");
-              output26State = "off";
-              digitalWrite(output26, LOW);
-            } else if (header.indexOf("GET /27/on") >= 0) {
-              Serial.println("GPIO 27 on");
-              output27State = "on";
-              digitalWrite(output27, HIGH);
-            } else if (header.indexOf("GET /27/off") >= 0) {
-              Serial.println("GPIO 27 off");
-              output27State = "off";
-              digitalWrite(output27, LOW);
+              digitalWrite(pumpOutput, HIGH);
+            } else if (header.indexOf("GET /25/off") >= 0) {
+              Serial.println("pump off");
+              pumpState = "off";
+              digitalWrite(pumpOutput, LOW);
             }
             
             // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            client.println("</head>"); 
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP32 Web Server TESTTTTT </h1>");  
-            
-            client.println("</body></html>");
+            client.println("<!DOCTYPE html>");
+            client.println("<html lang=\"en\">");
+            client.println("<head>");
+            client.println("  <meta charset=\"UTF-8\">");
+            client.println("  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            client.println("  <title>My Floating Garden</title>");
+            client.println("  <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">");
+            client.println("  <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>");
+            client.println("  <link href=\"https://fonts.googleapis.com/css2?family=Inclusive+Sans:ital,wght@0,300..700;1,300..700&family=Jua&family=Varela+Round&display=swap\" rel=\"stylesheet\">");
+            client.println("  <style>");
+            client.println("    :root {");
+            client.println("      --green1: #60693D;");
+            client.println("      --dark-brown: #69584C;");
+            client.println("      --tan: #C7B39A;");
+            client.println("      --off-white: #ECEBE8;");
+            client.println("      --beige: #D2C9BF;");
+            client.println("    }");
+            client.println("    .varela-round-regular {");
+            client.println("      font-family: 'Varela Round', sans-serif;");
+            client.println("      font-weight: 400;");
+            client.println("      font-style: normal;");
+            client.println("    }");
+            client.println("    .inclusive-sans {");
+            client.println("      font-family: 'Inclusive Sans', sans-serif;");
+            client.println("      font-optical-sizing: auto;");
+            client.println("      font-weight: 400;");
+            client.println("      font-style: normal;");
+            client.println("    }");
+            client.println("    .jua-regular {");
+            client.println("      font-family: 'Jua', sans-serif;");
+            client.println("      font-weight: 400;");
+            client.println("      font-style: normal;");
+            client.println("    }");
+            client.println("    html, body {");
+            client.println("      background-color: var(--off-white);");
+            client.println("      margin: 0;");
+            client.println("      padding: 0;");
+            client.println("      overflow-x: hidden;");
+            client.println("      width: 100%;");
+            client.println("      text-align: center;");
+            client.println("      display: flex;");
+            client.println("      flex-direction: column;");
+            client.println("      justify-content: center;");
+            client.println("    }");
+            client.println("    .main {");
+            client.println("      display: flex;");
+            client.println("      flex-direction: column;");
+            client.println("      justify-content: center;");
+            client.println("    }");
+            client.println("    .title {");
+            client.println("      font-family: 'Jua', sans-serif;");
+            client.println("      color: var(--green1);");
+            client.println("      margin: 0;");
+            client.println("      margin-top: 2rem;");
+            client.println("      font-size: 2rem;");
+            client.println("    }");
+            client.println("    .planter-info {");
+            client.println("      display: flex;");
+            client.println("      margin-top: 30px;");
+            client.println("      text-align: left;");
+            client.println("      gap: 70px;");
+            client.println("    }");
+            client.println("    .planter-title {");
+            client.println("      font-family: 'Jua', sans-serif;");
+            client.println("      color: var(--dark-brown);");
+            client.println("      font-size: 1.7rem;");
+            client.println("      margin: 0;");
+            client.println("    }");
+            client.println("    p {");
+            client.println("      font-family: 'Inclusive Sans', sans-serif;");
+            client.println("      margin: 0;");
+            client.println("      padding: 2px;");
+            client.println("    }");
+            client.println("    .garden-preview {");
+            client.println("      background-color: var(--tan);");
+            client.println("      height: 15rem;");
+            client.println("      width: 15rem;");
+            client.println("      margin: 2rem;");
+            client.println("      columns: 2;");
+            client.println("      gap: 16px;");
+            client.println("      padding: 16px;");
+            client.println("      border-radius: 20px;");
+            client.println("      place-self: center;");
+            client.println("    }");
+            client.println("    .garden-info {");
+            client.println("      place-self: center;");
+            client.println("    }");
+            client.println("    .planter-box {");
+            client.println("      background-color: var(--beige);");
+            client.println("      height: inherit;");
+            client.println("      border-radius: 10px;");
+            client.println("    }");
+            client.println("    .water-button {");
+            client.println("      border-radius: calc(infinity * 1px);");
+            client.println("      width: 70px;");
+            client.println("      height: 70px;");
+            client.println("      border-style: none;");
+            client.println("      background: var(--beige);");
+            client.println("    }");
+            client.println("    .water-button:active {");
+            client.println("      border-radius: calc(infinity * 1px);");
+            client.println("      width: 70px;");
+            client.println("      height: 70px;");
+            client.println("      border-style: none;");
+            client.println("      background: var(--tan);");
+            client.println("    }");
+            client.println("    .water-button-disabled {");
+            client.println("      border-radius: calc(infinity * 1px);");
+            client.println("      width: 70px;");
+            client.println("      height: 70px;");
+            client.println("      border-style: none;");
+            client.println("      background: #b8b8b8;");
+            client.println("    }");
+            client.println("  </style>");
+            client.println("</head>");
+            client.println("<body>");
+            client.println("  <section class=\"header\">");
+            client.println("    <h1 class=\"title\">My Floating Garden</h1>");
+            client.println("    <div>");
+            client.println("      <!-- Weather Icon goes here -->");
+            client.println("      <p>Vancouver, BC</p>");
+            client.println("    </div>");
+            client.println("  </section>");
+            client.println("  <section class=\"main\">");
+            client.println("    <div class=\"garden-preview\">");
+            client.println("      <div id=\"planter-box-1\" class=\"planter-box\"></div>");
+            client.println("      <div id=\"planter-box-2\" class=\"planter-box\"></div>");
+            client.println("    </div>");
+            client.println("    <div class=\"garden-info\">");
+            client.println("      <div id=\"planter-info-1\" class=\"planter-info\">");
+            client.println("        <div>");
+            client.println("          <h3 class=\"planter-title\">Sage Planter</h3>");
+            client.println("          <p>last watered: 26/03/2025</p>");
+            client.println("          <p>saturation: " + String(moisturePercent) + "%</p>");
+            client.println("        </div>");
+            if (pumpState == "off") { // if pump is inactive
+              client.println("        <button class=\"water-button\">");
+              client.println("          <p>water!</p>");
+              client.println("        </button>");
+            } else {                  // if pump is active
+              client.println("        <button class=\"water-button-disabled\">");
+              client.println("          <p>watering</p>");
+              client.println("        </button>");
+            }
+            client.println("          <p>water!</p>");
+            client.println("        </button>");
+            client.println("      </div>");
+            client.println("      <div id=\"planter-info-2\" class=\"planter-info\">");
+            client.println("        <div>");
+            client.println("          <h3 class=\"planter-title\">Planter 2</h3>");
+            client.println("          <p>last watered: 26/03/2025</p>");
+            client.println("          <p>saturation: " + String(moisturePercent) + "%</p>");
+            client.println("        </div>");
+            client.println("        <button class=\"water-button\">");
+            client.println("          <p>water!</p>");
+            client.println("        </button>");
+            client.println("      </div>");
+            client.println("    </div>");
+            client.println("  </section>");
+            client.println("  <section class=\"footer\">");
+            client.println("  </section>");
+            client.println("</body>");
+            client.println("</html>");
             
             // The HTTP response ends with another blank line
             client.println();
